1e7cf45caad755c3dde38b007f08eee6feb24cb5
Max Rosoff <me@maxrosoff.com>
2020-12-10 12:37:45 -0700

    Clean Up Repo Files

--- webpack.config.js
+++ webpack.config.js
@@ -6,35 +6,36 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const outputDirectory = "dist";
 
 module.exports = {
-	entry: ["@babel/polyfill", "./client/index.js"],
-	devServer: { port: 3000, open: true, hot: true, proxy: { "/api/*": "http://localhost:8080" } },
-	devtool: 'cheap-source-map',
+	entry: ["@babel/polyfill", "./site/index.js"],
+	devServer: {port: 3000, open: true, hot: true},
+	devtool: 'eval-source-map',
 	module: {
 		rules: [
-			{test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/react'] }},
-			{ test: /\.css$/i, use: ['style-loader', 'css-loader']},
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {presets: ['@babel/preset-env', '@babel/react']}
+			},
+			{test: /\.css$/i, use: ['style-loader', 'css-loader']},
 			{
 				test: /\.s[ac]ss$/i, use:
 					[
-						{ loader: 'style-loader' },
-						{ loader: 'css-loader' },
-						{ loader: 'postcss-loader', options:
-								{
-									plugins: function()
-									{
-										return [require('precss'), require('autoprefixer')];
-									}
-								}
-						},
-						{ loader: 'sass-loader' }
+						{loader: 'style-loader'},
+						{loader: 'css-loader'},
+						{loader: 'postcss-loader', options: {postcssOptions: {plugins: ['autoprefixer', 'precss']}}},
+						{loader: 'sass-loader'}
 					]
 			},
-			{test: /\.(png|svg|jpe?g|gif)$/, loader: ['file-loader']}
+			{test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpe?g|gif|mp4|wav|mp3)$/i, loader: 'file-loader'}
 		]
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({ template: "client/static/template/index.html", favicon: "client/static/template/favicon.ico"}),
+		new HtmlWebpackPlugin({
+			template: "./site/static/template/index.html",
+			favicon: "./site/static/template/favicon.ico"
+		}),
 		new webpack.HotModuleReplacementPlugin()
 	],
 	output: { filename: "bundle.js", path: path.join(__dirname, outputDirectory) },
