652748318c83b9f091cf27658994cd68d9135635
Max Rosoff <me@maxrosoff.com>
2020-12-23 15:26:54 -0700

    Remove unnecessary stuff, refactor and add new stuff

--- client/components/Home/Dashboard/Dashboard.js
+++ client/components/Home/Dashboard/Dashboard.js
@@ -1,18 +1,19 @@
 import React, {useEffect, useState} from "react";
 
-import {Avatar, Box, Button, Divider, Grid, IconButton, Paper, Typography} from "@material-ui/core";
+import {Box, CircularProgress, Grid, IconButton, Typography} from '@material-ui/core';
 import {grey} from "@material-ui/core/colors";
 
 import {makeStyles} from "@material-ui/core/styles";
 
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import ScheduleOutlinedIcon from '@material-ui/icons/ScheduleOutlined';
-import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
-import NavigateNextOutlinedIcon from '@material-ui/icons/NavigateNextOutlined';
+import OpacityIcon from '@material-ui/icons/Opacity';
+import WavesIcon from '@material-ui/icons/Waves';
+import DirectionsIcon from '@material-ui/icons/Directions';
 
-import AddIcon from '@material-ui/icons/Add';
+import {getSurf, getTidePrediction, getTideActual, getWeather} from '../../../hooks/useAPI';
 
-import Image from "../../../static/images/carousel1.jpg";
+import {CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
+import moment from 'moment';
 
 const useStyles = makeStyles(theme => ({
 	cardBox: {
@@ -30,19 +31,50 @@ const useStyles = makeStyles(theme => ({
 
 const DashBoard = props =>
 {
+	const [weatherData, setWeatherData] = useState();
+	const [tidePredictionData, setTidePredictionData] = useState();
+	const [tideActualData, setTideActualData] = useState();
+
+	useEffect(() =>
+	{
+		const getWeatherFromAPI = () =>
+		{
+			getWeather({key: '114e2f8559d9daba8a4ad4e51464c8b6'})
+			.then(r => setWeatherData(r.data));
+		};
+
+		const interval = setInterval(getWeatherFromAPI(), 60000);
+		return () => clearInterval(interval);
+	}, []);
+
+	useEffect(() =>
+	{
+		const getTidesFromAPI = () =>
+		{
+			getTidePrediction({}).then(r => setTidePredictionData(r.data));
+			getTideActual({}).then(r => setTideActualData(r.data));
+		}
+		const interval = setInterval(getTidesFromAPI(), 60000);
+		return () => clearInterval(interval);
+	}, [])
+
 	return (
 		<Grid container spacing={5}>
 			<Grid item>
 				<WidgetCard title={"Administration"}/>
 			</Grid>
 			<Grid item>
-				<WidgetCard title={"Weather"}/>
-			</Grid>
-			<Grid item>
-				<WidgetCard title={"Tide"}/>
-			</Grid>
-			<Grid item>
 				<WidgetCard title={"Surf"}/>
+			</Grid>
+			<Grid item>
+				<WidgetCard title={"Weather"}>
+					{weatherData ? <WeatherCard weatherData={weatherData}/> : <CircularProgress />}
+				</WidgetCard>
+			</Grid>
+			<Grid item style={{width: "100%"}}>
+				<WidgetCard title={"Tide"}>
+					{tidePredictionData ? <TideCard tidePredictionData={tidePredictionData} tideActualData={tideActualData}/> : <CircularProgress />}
+				</WidgetCard>
 			</Grid>
 		</Grid>
 
@@ -72,7 +104,7 @@ const WidgetCard = props =>
 				<Grid item>
 					<Grid container direction={"column"} spacing={4}>
 						<Grid item>
-							<Typography variant={"h6"}>{props.title}</Typography>
+							<Typography variant={"h3"}>{props.title}</Typography>
 						</Grid>
 						<Grid item>
 							{props.children}
@@ -82,6 +114,128 @@ const WidgetCard = props =>
 			</Grid>
 		</Box>
 	);
+}
+
+const WeatherCard = props =>
+{
+	return(
+		<Grid container direction={"column"} spacing={2}>
+			<Grid item>
+				<Grid container spacing={4}
+					  alignItems={"center"}
+				>
+					<Grid item>
+						<Grid container direction={"column"}>
+							<Grid item>
+								<Typography variant={"h4"}>{props.weatherData.main.temp + " °F"}</Typography>
+							</Grid>
+							<Grid item>
+								<Typography variant={"h5"}>{props.weatherData.weather[0].main}</Typography>
+							</Grid>
+						</Grid>
+					</Grid>
+					<Grid item>
+						<img
+							alt={"Weather Icon"}
+							src={"http://openweathermap.org/img/wn/" + props.weatherData.weather[0].icon + "@2x.png"}
+							style={{height: 140}}
+						/>
+					</Grid>
+				</Grid>
+			</Grid>
+			<Grid item>
+				<Grid container spacing={4}>
+					<Grid item>
+						<IconTextPair
+							icon={<OpacityIcon />}
+							text={props.weatherData.main.humidity + "%"}
+						/>
+					</Grid>
+					<Grid item>
+						<IconTextPair
+							icon={<WavesIcon />}
+							text={props.weatherData.wind.speed + " mi/h"}
+						/>
+					</Grid>
+					<Grid item>
+						<IconTextPair
+							icon={<DirectionsIcon />}
+							text={props.weatherData.wind.deg + "°"}
+						/>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	)
+}
+
+const IconTextPair = props =>
+{
+	return(
+		<Grid container spacing={1}
+			  justify={"center"} alignItems={"center"} alignContent={"center"}
+		>
+			<Grid item>
+				{props.icon}
+			</Grid>
+			<Grid item>
+				<Typography variant={"h6"}>{props.text}</Typography>
+			</Grid>
+		</Grid>
+	)
+}
+
+const TideCard = props =>
+{
+	if (!props.tidePredictionData || !props.tideActualData) return null;
+
+	const firstDataPointDate = new Date(props.tidePredictionData.predictions[0].t);
+
+	const startDate = new Date(firstDataPointDate.getFullYear(), firstDataPointDate.getMonth(), firstDataPointDate.getDate());
+	const endDate = new Date(firstDataPointDate.getFullYear(), firstDataPointDate.getMonth(), firstDataPointDate.getDate());
+
+	const predictionData = props.tidePredictionData.predictions.map(({t, v}) => ({time: new Date(t), timeNumber: new Date(t).getTime(), prediction: v}));
+	const actualData = props.tideActualData.data.map(({t, v}) => ({time: new Date(t), timeNumber: new Date(t).getTime(), actual: v}));
+
+	let data = predictionData.map(obj => ({...obj, ...actualData.find(item => item.timeNumber === obj.timeNumber)}));
+
+
+	return(
+		<Grid container direction={"column"} spacing={3}
+			justify={"center"} alignItems={"center"} alignContent={"center"}
+		>
+			<Grid item container spacing={4}
+				  justify={"center"} alignItems={"center"} alignContent={"center"}
+			>
+				<Grid item>
+					<Typography variant={"h6"}>Next Low Tide: 1:00 PM</Typography>
+				</Grid>
+				<Grid item>
+					<Typography variant={"h6"}>Next High Tide: 2:00 PM</Typography>
+				</Grid>
+			</Grid>
+			<Grid item style={{width: "100%"}}>
+				<ResponsiveContainer width={"100%"} height={350}>
+					<LineChart width={730} height={250} data={data}
+							   margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis
+							dataKey="timeNumber"
+							type="number"
+							scale="time"
+							interval="preserveStartEnd"
+							tickFormatter={tickItem => moment(tickItem).format('h:mm')}
+							domain={[startDate.getTime(), endDate.getTime()]}
+						/>
+						<YAxis />
+						<Legend />
+						<Line name="Predicted" type="monotone" dataKey="prediction" stroke="#8884d8" dot={false}/>
+						<Line name="Actual" type="monotone" dataKey="actual" stroke="#82ca9d" dot={false}/>
+					</LineChart>
+				</ResponsiveContainer>
+			</Grid>
+		</Grid>
+	)
 }
 
 export default DashBoard;
--- client/hooks/useAPI.js
+++ client/hooks/useAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const get = async(location, params) =>
+export const get = async (location, params) =>
 {
 	try
 	{
@@ -9,11 +9,11 @@ export const get = async(location, params) =>
 
 	catch(err)
 	{
-		return null
+		return null;
 	}
 }
 
-export const post = async(location, payload) =>
+export const post = async (location, payload) =>
 {
 	try
 	{
@@ -22,8 +22,58 @@ export const post = async(location, payload) =>
 
 	catch(err)
 	{
-		return null
+		return null;
 	}
 }
 
-export default {get, post};
+export const getSurf = async ({station = "92130", key}) =>
+{
+	try
+	{
+		return await axios.post(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=${key}`)
+	}
+
+	catch(err)
+	{
+		return null;
+	}
+}
+
+export const getTidePrediction = async ({station = "9410230"}) =>
+{
+	try
+	{
+		return await axios.post(`https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=today&station=${station}&product=predictions&datum=MLLW&time_zone=lst&units=english&format=json`);
+	}
+
+	catch(err)
+	{
+		return null;
+	}
+}
+
+export const getTideActual = async ({station = "9410230"}) =>
+{
+	try
+	{
+		return await axios.post(`https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=today&station=${station}&product=one_minute_water_level&datum=MLLW&time_zone=lst&units=english&format=json`);
+	}
+
+	catch(err)
+	{
+		return null;
+	}
+}
+
+export const getWeather = async ({zip = "92130", key}) =>
+{
+	try
+	{
+		return await axios.post(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=${key}`)
+	}
+
+	catch(err)
+	{
+		return null;
+	}
+}
